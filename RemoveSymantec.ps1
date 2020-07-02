46f085fc8ebfaa3cbf5480b4f5242d235e83cbe8
BartTechne <bart@techne.be>
2020-07-02 15:20:16 +0200

    Added RemoveSymantec

--- RemoveSymantec.ps1
+++ RemoveSymantec.ps1
@@ -1,2 +1,22 @@
-Remove-ItemProperty -Path "HKLM:\Software\Symantec"
-Remove-ItemProperty -Path "HKLM:\Software\WoW6432Node\Symantec"
+$symantec = Get-WmiObject -Class Win32_Product | Where-Object{$_.Name -eq "Symantec Endpoint Protection"}
+
+if ($symantec -eq '') {
+    Write-Host No Symantec installed
+    Exit 0
+
+} else {
+    $AppGUID = $symantec.properties["IdentifyingNumber"].value.toString()
+    Write-Host $AppGUID
+    msiexec.exe /norestart /q/x $AppGUID REMOVE=ALL
+    Get-WmiObject -Class Win32_Product | Where-Object{$_.Name -eq "Symantec Endpoint Protection"}
+        if ($symantec -eq '') {
+            Write-Host Uninstall Completed Reboot Needed
+            Exit 0
+        
+        } else {
+            Write-Host Uninstallation Not Succeeded
+            Exit 1
+        }
+}
+
+Set-ExecutionPolicy Unrestricted
